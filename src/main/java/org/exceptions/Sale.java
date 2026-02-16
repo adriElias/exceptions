01d59898c73d92eb675823f6e8d61c516b1fea77
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-16 18:41:33 +0100

    refactor: Update implementation of totalPrice calculation

--- src/main/java/org/exceptions/Sale.java
+++ src/main/java/org/exceptions/Sale.java
@@ -7,14 +7,19 @@ public class Sale {
     private double totalPrice = 0;
 
     public double calculateTotal() {
+        System.out.println(products.get(12));
         if (products.isEmpty()) {
             throw new EmptySaleException();
         }
 
-        for (Product product : products) {
-            totalPrice *= product.getPrice();
-        }
+        totalPrice = products.stream()
+                .mapToDouble(Product::getPrice)
+                .sum();
 
         return totalPrice;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
     }
 }
